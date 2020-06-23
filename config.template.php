c40bfa95dafa0e18133fa44ef6de21dbf0468da0
RundesBalli <rundesballi@rundesballi.com>
2020-06-23 19:45:17 +0200

    Added files

---
+++ config.template.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * config.php
+ * 
+ * Configuration file
+ */
+
+/**
+ * The text to be sent.
+ */
+$text = "";
+
+/**
+ * The URL of the image to be sent.
+ */
+$imageURL = "";
+
+/**
+ * Discord WebhookURL
+ */
+$webHookURL = "";
+?>

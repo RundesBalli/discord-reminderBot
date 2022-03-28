3a37b3669925cbe8454675fa86d05ec3a8da6906
RundesBalli <GitHub@RundesBalli.com>
2022-03-28 20:51:44 +0200

    Create bot.php

---
+++ Bot/bot.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Discord ReminderBot (Bot variant)
+ * 
+ * Sends a previously defined message to a discord text channel at defined intervals.
+ * 
+ * @author    RundesBalli <webspam@rundesballi.com>
+ * @copyright 2022 RundesBalli
+ * @version   1.0
+ * @license   MIT-License
+ * @see       https://github.com/RundesBalli/discord-reminderBot
+ */
+
+/**
+ * Including the configuration file.
+ */
+require_once(__DIR__.DIRECTORY_SEPARATOR."config.php");
+
+/**
+ * Initialize cURL
+*/
+$ch = curl_init();
+
+/**
+ * cURL options
+ * @see https://www.php.net/manual/en/function.curl-setopt.php
+ */
+$options = array(
+  CURLOPT_RETURNTRANSFER => TRUE,
+  CURLOPT_URL => "https://discord.com/api/channels/".$channelId."/messages",
+  CURLOPT_CONNECTTIMEOUT => 5,
+  CURLOPT_TIMEOUT => 10,
+  CURLOPT_HTTPHEADER => array('Content-Type: application/json', "Authorization: Bot ".$botToken),
+  CURLOPT_POST => TRUE
+);
+
+/**
+ * Data
+ */
+$data = array(
+  'content' => $text,
+  'embeds' => array(
+    array(
+      'image' => array(
+        'url' => $imageURL
+      )
+    )
+  )
+);
+$options[CURLOPT_POSTFIELDS] = json_encode($data);
+
+/**
+ * Insert the options-array into the cURL handle.
+ */
+curl_setopt_array($ch, $options);
+
+/**
+ * Execute the cURL handle.
+ */
+curl_exec($ch);
+
+/**
+ * Close the cURL handle.
+ */
+curl_close($ch);
+?>
